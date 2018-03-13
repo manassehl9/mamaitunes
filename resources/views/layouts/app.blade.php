5c82c40c779289ede31f94e28b9f102ba128ab8b
Manasseh Abiodun <manassehl9@gmail.com>
2018-03-13 23:18:46 +0100

    Modified the frontend then created the admin backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,64 +8,58 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Bootstrap core CSS     -->
+    <link href="{{asset('backend/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="{{asset('backend/css/material-dashboard.css?v=1.2.0') }}" rel="stylesheet" />
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="{{asset('backend/css/demo.css') }}" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+    @stack('css')
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+        <div class="wrapper">
+            @include('layouts.partial.sidebar')
+            <div class="main-panel">
+                @include('layouts.partial.topbar')
+                @yield('content')
+                @include('layouts.partial.footer')
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </div>
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <!--   Core JS Files   -->
+    <script src="{{asset('backend/js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
+    <script src="{{asset('backend/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{asset('backend/js/material.min.js') }}" type="text/javascript"></script>
+    <!--  Charts Plugin -->
+    <script src="{{asset('backend/js/chartist.min.js') }}"></script>
+    <!--  Dynamic Elements plugin -->
+    <script src="{{asset('backend/js/arrive.min.js') }}"></script>
+    <!--  PerfectScrollbar Library -->
+    <script src="{{asset('backend/js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <!--  Notifications Plugin    -->
+    <script src="{{asset('backend/js/bootstrap-notify.js') }}"></script>
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Material Dashboard javascript methods -->
+    <script src="{{asset('backend/js/material-dashboard.js?v=1.2.0') }}"></script>
+    <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+    <script src="{{asset('backend/js/demo.js') }}"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            // Javascript method's body can be found in assets/js/demos.js
+            demo.initDashboardPageCharts();
+
+        });
+    </script>
+    @stack('scripts')
 </body>
 </html>
