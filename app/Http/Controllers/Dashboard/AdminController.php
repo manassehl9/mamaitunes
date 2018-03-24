77fbadd2707ac834b2a6dbd48c66395ebabf882d
Manasseh Abiodun <manassehl9@gmail.com>
2018-03-24 21:26:58 +0100

    made final changes to the dashboard

--- app/Http/Controllers/Dashboard/AdminController.php
+++ app/Http/Controllers/Dashboard/AdminController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Category;
+use App\Slider;
+use App\Item;
+use App\Contact;
+use App\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    public function index(){
-        return view('dashboard.admin');
+    public function index()
+    {
+        $categoryCount = Category::count();
+        $itemCount = Item::count();
+        $sliderCount = Slider::count();
+        $contactCount = Contact::count();
+        $reservations = Reservation::where('status',false)->get();
+        return view('dashboard.admin', compact('categoryCount', 'itemCount', 'reservations', 'contactCount', 'sliderCount'));
     }
 }
